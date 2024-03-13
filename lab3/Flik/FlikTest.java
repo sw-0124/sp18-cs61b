2145a677f33e07ffc4ba8fa62abb845c5c6a383c
tomato <2535981850@qq.com>
2024-03-13 21:12:23 +0800

    add FlikTest.java

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,8 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class FlikTest {
+    @Test
+    public void testIsSameNumber(){
+        Assert.assertTrue(Flik.isSameNumber(1, 1));
+    }
+}
